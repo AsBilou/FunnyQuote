9075582a95da415278da8e43f49830a22f066ed5
Romain Bellina <contact@romainbellina.fr>
2013-03-24 23:56:11 +0100

    Add comment admin part

--- funnyquotes-admin.php
+++ funnyquotes-admin.php
@@ -2,11 +2,13 @@
 
 add_action('admin_menu','funny_quotes_admin_menu');
 
+/* Création de la fonction qui ajoutera les elements au menu*/
 function funny_quotes_admin_menu(){
     add_menu_page('Funny Quotes Options','Funny Quotes','manage_options','funny_quotes','funny_quotes_options','images/comment-grey-bubble.png');
     add_submenu_page('funny_quotes','Ajouter quotes','Ajouter quotes','manage_options','add_funny_quotes','add_funny_quotes_options');
 }
 
+/* Fonction qui affiche la liste des quotes enregistré */
 function funny_quotes_options() {
     if ( !current_user_can( 'manage_options' ) )  {
         wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
@@ -16,7 +18,7 @@ function funny_quotes_options() {
     echo '</div>';
 }
 
-
+/* Fonction qui affiche le formulaire pour créer un quote */
 function add_funny_quotes_options() {
     if ( !current_user_can( 'manage_options' ) )  {
         wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
@@ -27,28 +29,32 @@ function add_funny_quotes_options() {
     echo '</div>';
 }
 
+//Fonction qui gere l'affiche de la liste des quotes
 function funny_quotes_display(){
 
-    //Our class extends the WP_List_Table class, so we need to make sure that it's there
+    //Ajout de la classe WP_List_Table class
     if(!class_exists('WP_List_Table')){
         require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
     }
-
+    //Ajout de la classe liste_quote
     require_once(ABSPATH . 'wp-content/plugins/FunnyQuotes/liste_quote.php' );
 
-    //Prepare Table of elements
+    //Prepare le tableau des éléments
     $wp_list_table = new liste_quote();
     $wp_list_table->prepare_items();
 
-    //Table of elements
+    //Affiche le tableau
     $wp_list_table->display();
 }
 
+//Fonction qui gere l'affichage de la page d'ajout de quote.
 function funny_quotes_add_display(){
 
     global $wpdb;
-
+    
+    //Lors de la supression
     if($_GET['action']=='delete'){
+        //Apres vérification javascript, on supprime l'entré dans la base de données
         $id = $_GET['quote'];
         $table_name = $wpdb->prefix . "funny_quotes";
 
@@ -62,8 +68,10 @@ function funny_quotes_add_display(){
             <a class="button" href="admin.php?page=add_funny_quotes">Ajouter une nouvelle quote</a>
         <?php
 
-    }elseif($_GET['action']=='edit'){
-        if($_POST){
+    }elseif($_GET['action']=='edit'){//Lors de l'édition
+        if($_POST){//Apres envoie du formulaire de la quote édité
+        
+            //On crée une requete et on l'envoi a la base de données.
             $id = $_GET['quote'];
             $quote = $_POST['newcitation'];
             $author = $_POST['author'];
@@ -79,7 +87,7 @@ function funny_quotes_add_display(){
                 <a class="button" href="admin.php?page=add_funny_quotes">Ajouter une nouvelle quote</a>
             <?php
 
-        }else{
+        }else{// Affichage du formulaire d'édition avec les doonées pré-remplis.
             $id = $_GET['quote'];
 
             $query = "SELECT * FROM ".$wpdb->prefix . "funny_quotes WHERE id = ".$id.";";
@@ -100,9 +108,11 @@ function funny_quotes_add_display(){
             <?php
         }
     }
-
+    
+    //Si on envoi le forumaire de création
     if($_POST && !$_GET['action']){
-
+        
+        //On crée la requete et on l'envoi a la base de données.
         $table_name = $wpdb->prefix . "funny_quotes";
 
         $wpdb->insert($table_name , array('author' => $_POST['author'] ,
@@ -113,7 +123,7 @@ function funny_quotes_add_display(){
             <a class="button" href="admin.php?page=add_funny_quotes">Ajouter une nouvelle quote</a>
         <?php
     }
-    elseif(!$_GET['action']){
+    elseif(!$_GET['action']){//Affichage du formulaire vide pour ajouter une nouvelle quote.
         ?>
         <h2>Ajouter une nouvelle citation drôle !</h2>
 
