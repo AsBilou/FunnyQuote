346e3cd886daeee0932da8989d2841b1ee3fd1c9
Romain Bellina <contact@romainbellina.fr>
2013-03-25 00:14:50 +0100

    Add comments class liste_quote

--- liste_quote.php
+++ liste_quote.php
@@ -3,9 +3,8 @@
 
 class liste_quote extends WP_List_Table {
 
-    /**
-     * Constructor, we override the parent to pass our own arguments
-     * We usually focus on three parameters: singular and plural labels, as well as whether the class supports AJAX.
+    /*
+     * Constructeur de la classe
      */
     function __construct() {
         parent::__construct( array(
@@ -15,9 +14,8 @@ class liste_quote extends WP_List_Table {
         ) );
     }
 
-    /**
-     * Add extra markup in the toolbars before or after the list
-     * @param string $which, helps you decide if you add the markup after (bottom) or before (top) the list
+    /*
+     * Gestion de l'entete et pied de page
      */
     function extra_tablenav( $which ) {
         if ( $which == "top" ){
@@ -27,8 +25,7 @@ class liste_quote extends WP_List_Table {
     }
 
     /**
-     * Define the columns that are going to be used in the table
-     * @return array $columns, the array of columns to use with the table
+     * Définie les colonnes a afficher dans le tableau
      */
     function get_columns() {
         return $columns= array(
@@ -40,8 +37,7 @@ class liste_quote extends WP_List_Table {
     }
 
     /**
-     * Decide which columns to activate the sorting functionality on
-     * @return array $sortable, the array of columns that can be sorted by the user
+     * Défini les colonnes qui sont triable
      */
     public function get_sortable_columns() {
         return $sortable = array(
@@ -50,88 +46,80 @@ class liste_quote extends WP_List_Table {
         );
     }
 
-    /**
-     * Prepare the table with different parameters, pagination, columns and table elements
+    /*
+     * Organise les paramatres pour les afficher
      */
     function prepare_items() {
         global $wpdb, $_wp_column_headers;
         $screen = get_current_screen();
 
-        /* -- Preparing your query -- */
+        /* -- Création de la requete -- */
         $table_name = $wpdb->prefix . "funny_quotes";
         $query="SELECT * FROM ".$table_name;
 
-        /* -- Ordering parameters -- */
-        //Parameters that are going to be used to order the result
+        /* -- Récuperation des colonnes a trier -- */
+
         $orderby = !empty($_GET["orderby"]) ? mysql_real_escape_string($_GET["orderby"]) : 'ASC';
         $order = !empty($_GET["order"]) ? mysql_real_escape_string($_GET["order"]) : '';
         if(!empty($orderby) & !empty($order)){ $query.=' ORDER BY '.$orderby.' '.$order; }
 
-        /* -- Pagination parameters -- */
-        //Number of elements in your table?
-        $totalitems = $wpdb->query($query); //return the total number of affected rows
+        /* -- Récuperation du nombre total de quote -- */
+        
+        $totalitems = $wpdb->query($query);
 
-        //How many to display per page?
+        //Nombre de quote par page
         $perpage = 10;
-        //Which page is this?
+        //Gestion des pages
         $paged = !empty($_GET["paged"]) ? mysql_real_escape_string($_GET["paged"]) : '';
-        //Page Number
+        
         if(empty($paged) || !is_numeric($paged) || $paged<=0 ){ $paged=1; }
-        //How many pages do we have in total?
+
         $totalpages = ceil($totalitems/$perpage);
-        //adjust the query to take pagination into account
         if(!empty($paged) && !empty($perpage)){
             $offset=($paged-1)*$perpage;
             $query.=' LIMIT '.(int)$offset.','.(int)$perpage;
         }
 
-        /* -- Register the pagination -- */
         $this->set_pagination_args( array(
             "total_items" => $totalitems,
             "total_pages" => $totalpages,
             "per_page" => $perpage,
         ) );
-        //The pagination links are automatically built according to those parameters
 
-        /* — Register the Columns — */
+        /* — Gestions des colonnes — */
         $columns = $this->get_columns();
         $hidden = array();
         $sortable = $this->get_sortable_columns();
         $this->_column_headers = array($columns, $hidden, $sortable);
 
-        /* -- Fetch the items -- */
+        /* -- Récupération des quotes -- */
         $this->items = $wpdb->get_results($query);
     }
 
-    /**
-     * Display the rows of records in the table
-     * @return string, echo the markup of the rows
+    /*
+     * Affiche les informations
      */
     function display_rows() {
 
-        //Get the records registered in the prepare_items method
+        //Récuperation des quotes
         $records = $this->items;
 
-        //Get the columns registered in the get_columns and get_sortable_columns methods
+        //Récuperation des colonnes
         list( $columns, $hidden ) = $this->get_column_info();
-        //Loop for each record
         if(!empty($records)){
+            //pour chaque quote
             foreach($records as $rec){
-                //Open the line
 
                 echo '<tr id="record_'.$rec->id.'">';
                 foreach ( $columns as $column_name => $column_display_name ) {
 
-                    //Style attributes for each col
                     $class = "class='$column_name column-$column_name'";
                     $style = "";
                     if ( in_array( $column_name, $hidden ) ) $style = ' style="display:none;"';
                     $attributes = $class . $style;
 
-                    //edit link
                     $editlink  = '/wp-admin/link.php?action=edit&id='.(int)$rec->id;
 
-                    //Display the cell
                     switch ( $column_name ) {
                         case "col_id":	echo '<td '.$attributes.'>'.stripslashes($rec->id).'</td>';	break;
                         case "col_author": echo '<td '.$attributes.'>'.stripslashes($rec->author).'</td>'; break;
@@ -141,7 +129,6 @@ class liste_quote extends WP_List_Table {
 
                 }
 
-                //Close the line
                 echo'</tr>';
 
                 }
